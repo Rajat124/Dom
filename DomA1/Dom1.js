f98bc45f38bc316ee541e6f63a4a8d505e46a158
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-05-19 18:28:33 +0530

    3th ele. color change

--- DomA1/Dom1.js
+++ DomA1/Dom1.js
@@ -1,5 +1,11 @@
 var headTitle = document.getElementById("header-title");
-headTitle.innerHTML = "Hello";
+// headTitle.innerHTML = "Hello";
+
+console.log(headTitle.innerText);
+console.log(headTitle.textContent);
+
+// innertext focus on CSS while textContent does not.
+
 headTitle.style.background = "red";
 headTitle.style.border = "solid 5px black";
 
@@ -8,8 +14,19 @@ var mainHeader = document.getElementById("main-header");
 mainHeader.style.borderBottom = "solid 5px blue";
 
 var addItem = document.getElementsByClassName("title");
-console.log(addItem);
 
 addItem[0].style.backgroundColor = "yellow";
 addItem[0].style.fontWeight = "bold";
 addItem[0].style.color = "green";
+
+var listItem = document.getElementsByClassName("list-group-item");
+console.log(listItem);
+
+listItem[2].style.backgroundColor = "green";
+listItem[0].style.fontWeight = "bold";
+listItem[1].style.fontWeight = "bold";
+listItem[3].style.fontWeight = "bold";
+
+// for (let i = 0; i < listItem.length; i++) {
+//   listItem[i].style.backgroundColor = "red";
+// }
