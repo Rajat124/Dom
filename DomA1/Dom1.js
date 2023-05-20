9a77f5c7b02a636a4b2d40e8f9ae138dddaee029
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-05-20 10:43:22 +0530

    QuerySelector changes

--- DomA1/Dom1.js
+++ DomA1/Dom1.js
@@ -1,4 +1,4 @@
-/*/ GetElementByID
+/* / GetElementByID
 
 var headTitle = document.getElementById("header-title");
 // headTitle.innerHTML = "Hello";
@@ -38,9 +38,39 @@ listItem[3].style.backgroundColor = "green";
 // }
 */
 
+/*
 var listFive = document.getElementsByClassName("list-5");
 listFive[0].style.color = "green";
 listFive[0].style.fontWeight = "bold";
 
 var listFive1 = document.getElementsByTagName("li");
 listFive1[4].style.backgroundColor = "red";
+
+console.log(listFive);
+console.log(listFive1);
+*/
+
+/* // GetElementByTagName
+
+var liItem = document.getElementsByTagName("li");
+liItem[0].style.backgroundColor = "yellow";
+liItem[0].style.fontWeight = "bold";
+liItem[0].style.color = "green";
+
+liItem[2].style.backgroundColor = "green";
+liItem[0].style.fontWeight = "bold";
+liItem[1].style.fontWeight = "bold";
+liItem[3].style.fontWeight = "bold";
+
+for (let i = 0; i < liItem.length; i++) {
+  liItem[i].style.backgroundColor = "red";
+}
+*/
+
+// QuerySelector
+
+var item2 = document.querySelector(".list-group-item:nth-child(2)");
+item2.style.backgroundColor = "green";
+
+var item3 = document.querySelector(".list-group-item:nth-child(3)");
+item3.style.display = "none";
