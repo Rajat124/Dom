c4b63edf1bda5cf40236dcd35d5f8ad332a851c1
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-05-20 09:05:47 +0530

    Changes with Tag & ClassName

--- DomA1/Dom1.js
+++ DomA1/Dom1.js
@@ -1,3 +1,5 @@
+/*/ GetElementByID
+
 var headTitle = document.getElementById("header-title");
 // headTitle.innerHTML = "Hello";
 
@@ -10,23 +12,35 @@ headTitle.style.background = "red";
 headTitle.style.border = "solid 5px black";
 
 var mainHeader = document.getElementById("main-header");
-
 mainHeader.style.borderBottom = "solid 5px blue";
+*/
+
+/* // GetElementByClassName
 
 var addItem = document.getElementsByClassName("title");
-
 addItem[0].style.backgroundColor = "yellow";
 addItem[0].style.fontWeight = "bold";
 addItem[0].style.color = "green";
 
 var listItem = document.getElementsByClassName("list-group-item");
+// var listItem1 = document.getElementById("items");
 console.log(listItem);
+// console.log(listItem1);
 
 listItem[2].style.backgroundColor = "green";
 listItem[0].style.fontWeight = "bold";
 listItem[1].style.fontWeight = "bold";
 listItem[3].style.fontWeight = "bold";
+listItem[3].style.backgroundColor = "green";
 
 // for (let i = 0; i < listItem.length; i++) {
 //   listItem[i].style.backgroundColor = "red";
 // }
+*/
+
+var listFive = document.getElementsByClassName("list-5");
+listFive[0].style.color = "green";
+listFive[0].style.fontWeight = "bold";
+
+var listFive1 = document.getElementsByTagName("li");
+listFive1[4].style.backgroundColor = "red";
